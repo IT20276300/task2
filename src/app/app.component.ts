4e77905618ba95f32d53da6dd34c1339200c429f
it20276300 <it20276300@my.sliit.lk>
2021-12-31 17:26:48 +0530

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'task2';
+  firstNo!:number;
+  secondNo!:number;
+  result!:number;
+
+  Answer ():void{
+    //this result =parseInt(this.nvalue)+parseInt(this value)
+    console.log(this.firstNo,this.secondNo)
+    this.result=this.secondNo*Math.pow(this.firstNo,this.secondNo-1)
+
+  }
+    
+  
+ 
+  
 }
